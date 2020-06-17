48d2550e85a94d346895c70a70cd93f12a08913f
prinsapps <prinsapps@gmail.com>
2020-06-17 08:50:52 +0100

    navbar added

---
+++ src/components/navbar.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Monitor-Exercises</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Exercises</Link>
+          </li>
+
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create Students</Link>
+          </li>
+
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create Monitor-Exercises Log</Link>
+          </li>
+
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
